5525c2d7eea58f4ea59f4f812d398f41dcaa8c31
Rafiabiassyarif <rafiabiassyarif@gmail.com>
2025-11-25 10:11:22 +0700

    neww

--- database/seeders/CommunityPostSeeder.php
+++ database/seeders/CommunityPostSeeder.php
@@ -9,33 +9,30 @@ class CommunityPostSeeder extends Seeder
 {
     public function run(): void
     {
-        
         CommunityPost::create([
             'community_id' => 1,
-            'user_id'      => 1,
+            'author_name'  => 'Faisal Rahman',
             'content'      => 'Halo! Ini postingan pertama di komunitas Fotografer Alam.',
             'image'        => null,
         ]);
 
         CommunityPost::create([
             'community_id' => 1,
-            'user_id'      => 2,
+            'author_name'  => 'Siti Nurhaliza',
             'content'      => 'Berbagi foto lanskap terbaru saya! Bagaimana menurut kalian?',
             'image'        => 'lanskap1.jpg',
         ]);
 
-       
         CommunityPost::create([
             'community_id' => 2,
-            'user_id'      => 3,
+            'author_name'  => 'Budi Pratama',
             'content'      => 'Tanaman monstera saya tumbuh subur! Ada yang mau tips rawatan?',
             'image'        => null,
         ]);
 
-        
         CommunityPost::create([
             'community_id' => 3,
-            'user_id'      => 4,
+            'author_name'  => 'Ali Hakim',
             'content'      => 'Bagaimana pendapat kalian soal desain UI button berbasis neumorphism?',
             'image'        => null,
         ]);
